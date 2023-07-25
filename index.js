f8610d237b7bb89f71f2c5e8d7abc7610c521791
Szymon Wilczek <kazikwilczek7@gmail.com>
2023-07-25 22:50:53 +0200

    Lepsze sortowanie zadań

--- index.js
+++ index.js
@@ -17,11 +17,26 @@ app.get("/random-problem", (req, res) => {
   }
 });
 
+app.get("/random-problems", (req, res) => {
+  try {
+    let allProblems = problems.sort(() => Math.random() - 0.5);
+
+    //random number of problems in range 5-10
+    const count = Math.floor(Math.random() * (10 - 5 + 1) + 5);
+    allProblems = allProblems.slice(0, count);
+
+    res.json({ zadania: allProblems });
+  } catch (error) {
+    res.status(500).json({ error: "Błąd serwera" });
+  }
+});
+
 app.get("/problems", (req, res) => {
   try {
     const typesParam = req.query.types;
     const countParam = req.query.count;
     const maturaParam = req.query.matura;
+    const difficultyParam = req.query.difficulty; // Nowy parametr
 
     problems.sort(() => Math.random() - 0.5);
 
@@ -51,38 +66,31 @@ app.get("/problems", (req, res) => {
       ];
 
       return res.json({ zadania: maturaProblems });
-    }
-
-    if (typesParam && typesParam.toLowerCase() === "otwarte") {
-      let otwarteProblems = problems.filter(
-        (problem) => problem.type === "otwarte"
-      );
-
-      if (countParam) {
-        const count = parseInt(countParam);
-        otwarteProblems = otwarteProblems.slice(0, count);
-      }
-
-      res.json({ zadania: otwarteProblems });
-    } else if (typesParam && typesParam.toLowerCase() === "zamkniete") {
-      let zamknieteProblems = problems.filter(
-        (problem) => problem.type === "zamkniete"
-      );
-
-      if (countParam) {
-        const count = parseInt(countParam);
-        zamknieteProblems = zamknieteProblems.slice(0, count);
-      }
-
-      res.json({ zadania: zamknieteProblems });
     } else {
+      let filteredProblems = problems;
+
+      if (typesParam && typesParam.toLowerCase() === "otwarte") {
+        filteredProblems = filteredProblems.filter(
+          (problem) => problem.type === "otwarte"
+        );
+      } else if (typesParam && typesParam.toLowerCase() === "zamkniete") {
+        filteredProblems = filteredProblems.filter(
+          (problem) => problem.type === "zamkniete"
+        );
+      }
+
+      if (difficultyParam) {
+        filteredProblems = filteredProblems.filter(
+          (problem) => problem.difficulty === difficultyParam
+        );
+      }
+
       if (countParam) {
         const count = parseInt(countParam);
-        const allProblems = problems.slice(0, count);
-        res.json({ zadania: allProblems });
-      } else {
-        res.json({ zadania: problems });
+        filteredProblems = filteredProblems.slice(0, count);
       }
+
+      res.json({ zadania: filteredProblems });
     }
   } catch (error) {
     res.status(500).json({ error: "Błąd serwera" });
